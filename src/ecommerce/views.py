3b2616b68b622e5e7c4e4b32de4f7ac29bc37bcf
tomaszjankrawczyk <Asternacja12>
2020-10-10 18:20:32 +0200

    Initial commit

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,3 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def home_page(request):
+    context = {
+        "title":"Hello World!"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title":"About page"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title":"Contact page"
+    }
+    return render(request, "home_page.html", context)
