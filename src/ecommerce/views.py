e8b5e796aa412fe05abc12d123dac08011b0485a
tomaszjankrawczyk <Asternacja12>
2020-10-10 19:11:13 +0200

    Initial commit

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,18 +3,26 @@ from django.shortcuts import render
 
 def home_page(request):
     context = {
-        "title":"Hello World!"
+        "title":"Hello World!",
+        "content":"Welcome to the homepage"
     }
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
-        "title":"About page"
+        "title":"About page",
+        "content":"Welcome to the about page"
     }
     return render(request, "home_page.html", context)
 
 def contact_page(request):
     context = {
-        "title":"Contact page"
+        "title":"Contact page",
+        "content":"Welcome to the contact page"
     }
-    return render(request, "home_page.html", context)
+    if request.method == "POST":
+        print(request.POST)
+        print(request.POST["full_name"])
+        print(request.POST["email"])
+        print(request.POST["content"])
+    return render(request, "contact/view.html", context)
