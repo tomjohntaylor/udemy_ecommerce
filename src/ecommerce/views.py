96619810413af1c6f26c13520ec387c741bae86a
tomaszjankrawczyk <Asternacja12>
2020-10-10 20:47:35 +0200

    `Initial commit`

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,6 +3,21 @@ from django.shortcuts import render
 
 from .forms import ContactForm
 
+import requests
+from django.http import StreamingHttpResponse
+from django.contrib.auth.views import login_required
+
+#@login_required
+def download(request):
+    # handle user custom user permissions
+    url = 'file_url_here'
+    filename = os.path.basename(url)
+    r = requests.get(url, stream=True)
+    response = StreamingHttpResponse(streaming_content=r)
+    response['Content-Disposition'] = f'attachement; filename="{filename}"'
+    return response
+
+
 def home_page(request):
     context = {
         "title":"Hello World!",
@@ -30,3 +45,4 @@ def contact_page(request):
         print(request.POST["email"])
         print(request.POST["content"])
     return render(request, "contact/view.html", context)
+
