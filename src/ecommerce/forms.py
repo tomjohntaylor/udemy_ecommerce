47bad0f2aeb9b10efc152554df96f48d1cd9a410
tomaszjankrawczyk <Asternacja12>
2020-10-11 09:16:14 +0200

    `Initial commit`

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -2,6 +2,27 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField()
-    email = forms.EmailField()
-    content = forms.CharField()
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your full name"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your full email"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your full content"
+            }
+        )
+    )
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -6,29 +6,32 @@ from .forms import ContactForm
 
 def home_page(request):
     context = {
-        "title":"Hello World!",
-        "content":"Welcome to the homepage"
+        "title": "Hello World!",
+        "content": "Welcome to the homepage"
     }
     return render(request, "home_page.html", context)
+
 
 def about_page(request):
     context = {
-        "title":"About page",
-        "content":"Welcome to the about page"
+        "title": "About page",
+        "content": "Welcome to the about page"
     }
     return render(request, "home_page.html", context)
 
-def contact_page(request):
-    contact_form = ContactForm()
-    context = {
-        "title":"Contact page",
-        "content":"Welcome to the contact page",
-        "form":contact_form
-    }
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST["full_name"])
-        print(request.POST["email"])
-        print(request.POST["content"])
-    return render(request, "contact/view.html", context)
 
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)    # if there POST data pass it if None pass None
+    context = {
+        "title": "Contact page",
+        "content": "Welcome to the contact page",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     print(request.POST)
+    #     print(request.POST["fullname"])
+    #     print(request.POST["email"])
+    #     print(request.POST["content"])
+    return render(request, "contact/view.html", context)
