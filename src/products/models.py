20574b4c1f6062da6f71f3326fc135e4e67986a8
tomaszjankrawczyk <Asternacja12>
2020-10-12 21:22:48 +0200

    products app

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title       = models.CharField(max_length=120)
+    description = models.TextField()
+    price       = models.DecimalField(max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
